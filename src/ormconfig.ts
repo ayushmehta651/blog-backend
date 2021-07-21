be28925e56ec1c2411b0b4179260e7e6446a4a6c
Ayush mehta <ayushmehta651@gmail.com>
2021-07-21 19:05:29 +0530

    Added DB Credentials

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -1,19 +1,22 @@
 import { join } from "path";
 import { ConnectionOptions } from "typeorm";
 import { User } from "./database/user/entity/user.entity";
-import { FollowingEntity } from './database/following/entity/following.entity';
+import { FollowingEntity } from "./database/following/entity/following.entity";
 import { UserPostsEntity } from "./database/user_info/entity/user_info.entity";
-import { FollowerEntity } from './database/follower/entity/follower.entity';
+import { FollowerEntity } from "./database/follower/entity/follower.entity";
+import dotenv from "dotenv";
 
-
+dotenv.config();
 const connectionOptions: ConnectionOptions = {
+  url: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
   type: "postgres",
-  host: "localhost",
-  port: 5432,
-  username: "postgres",
-  password: "password",
-  database: "postgres",
-  entities: [User, FollowingEntity,UserPostsEntity, FollowerEntity],
+  host: process.env.Host || "localhost",
+  port: 5432 || process.env.DB_PORT,
+  username: process.env.User || "postgres",
+  password: process.env.DB_Password || "password",
+  database: process.env.Database || "postgres",
+  entities: [User, FollowingEntity, UserPostsEntity, FollowerEntity],
   synchronize: true,
   dropSchema: false,
   migrationsRun: true,
